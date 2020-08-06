a576a30695b9e958e14753bbea86e7ce328eed9d
Yahya Aloyoni <39294093+YA7MO@users.noreply.github.com>
2020-08-06 19:28:17 +1000

    Update M1.T3C.ino

--- module1/M1.T3C.ino
+++ module1/M1.T3C.ino
@@ -1,31 +1,38 @@
-volatile byte LED1 = 0;
-volatile byte LED2 = 0;
+int LED1 = 13;
+int LED2 = 12;
+int PIR1 = 2;
+int PIR2 = 3;
+
+int ledState = LOW;
+
 void setup()
 {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
   
+  attachInterrupt(digitalPinToInterrupt(PIR1), blue, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIR2), red, RISING);
   
-  PCICR |=0b00000111; //enables the interrupt for the ports
-  PCMSK2 |= 0b10001100;// enables interrupts on more than one pin.
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(PIR1,INPUT);
+  pinMode(PIR2,INPUT);
+  
 }
 
 void loop()
 {
- 
-  digitalWrite(13,LED1);
-  digitalWrite(12,LED2);
-  Serial.println("in loop");
-  delay(500);
-// Block of code.
+ // block of code
+  
 }
 
-ISR(PCINT2_vect){
-  LED1 = PIND & B00000100;
-  LED2 = PIND & B00001000;
-  Serial.println("interrupted");
+void blue(){
+  Serial.println("PIR1");
+  ledState = !ledState;
+  digitalWrite(LED1,ledState);
+}
+
+void red(){
+  Serial.println("PIR2");
+  ledState = !ledState;
+  digitalWrite(LED2,ledState);
 }
